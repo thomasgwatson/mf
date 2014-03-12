03b9c5c35c6241cf2d0dd290ba7a7f8ba3924ef6
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-11 23:24:39 -0700

    Solution for switching the values in an array of characters

---
+++ switch_array_in_place.rb
@@ -0,0 +1,16 @@
+# Write a function to reverse an array of characters in place.
+# "In place" means "without creating a new string in memory."
+
+array_of_chars = ["a", "b", "c", "d", "e"]
+
+def reverse_char(string)
+  operations = string.length/2
+  end_of_array = -1
+  for i in (0...operations)
+    string[i], string[end_of_array] = string[end_of_array], string[0]
+    end_of_array -= 1
+  end
+  string
+end
+
+p reverse_char(array_of_chars)
