ac285398a9a8367acd42f37a9acd2ec317dbe455
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-12 14:21:26 -0700

    More efficient solution

--- queue_with_two_stacks.rb
+++ queue_with_two_stacks.rb
@@ -2,36 +2,28 @@ class Queue
   def initialize
     @in = []
     @out = []
-    @mode = "in"
   end
 
   def return_queue
-    return @in if @mode == 'in'
-    return @out if @mode == 'out'
+    return [@in, @out]
   end
 
   def enqueue(value)
-    self.switch_stacks if @mode == 'out'
     @in.push(value)
   end
 
   def dequeue
-    # @out.pop if @out
-    self.switch_stacks if @mode == "in"
-    @out.pop
+    if @out.empty?
+      self.empty_in_stack
+      @out.pop
+    else
+      @out.pop
+    end
   end
 
-  def switch_stacks
-    if @mode == "in"
-      @in.length.times do
-        @out.push(@in.pop)
-      end
-      @mode = "out"
-    else
-      @out.length.times do
-        @in.push(@out.pop)
-      end
-      @mode = "in"
+  def empty_in_stack
+    @in.length.times do
+      @out.push(@in.pop)
     end
   end
 end
@@ -42,11 +34,10 @@ for i in (1..7) do
   test_queue.enqueue(i)
 end
 
-p 'yay' if test_queue.return_queue == [1,2,3,4,5,6,7]
+p 'yay' if test_queue.return_queue == [[1,2,3,4,5,6,7], []]
 p "works in this instance" if test_queue.dequeue == 1
-p 'woo' if test_queue.enqueue(8) == [2,3,4,5,6,7,8]
 test_queue.dequeue
 test_queue.dequeue
 p test_queue.return_queue
-test_queue.enqueue(9)
+test_queue.enqueue(8)
 p test_queue.return_queue
