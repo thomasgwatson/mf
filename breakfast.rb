0364b95a7e777166e2ddef68a43f0e03e0405dc0
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-18 15:59:20 -0700

    Implemented eat_breakfast

--- breakfast.rb
+++ breakfast.rb
@@ -1,2 +1,23 @@
 class Breakfast
+  attr_reader :food
+  attr_accessor :time
+  def initialize(args={})
+    @food = args[:food] || {}
+    @time = args[:time] || 0
+  end
+
+  def eat_breakfast
+    if @time > 60 && @food[:porridge] > 0
+       make(:porridge)
+    elsif @time > 20 && @food[:bagels] > 0
+       make(:bagels)
+    else
+      "Tom is starving :("
+    end
+  end
+
+  def make(food)
+    @food[food] -= 1
+    return "Delicious #{food} consumed!"
+  end
 end
