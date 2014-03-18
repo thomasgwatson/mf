dca5a8e0da2ec4079646b8cc336b6412be5f6eeb
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-18 15:51:00 -0700

    Finished up basic spec for breakfast

--- breakfast_spec.rb
+++ breakfast_spec.rb
@@ -4,8 +4,9 @@ require_relative 'breakfast'
 describe Breakfast do
   context "when there is lots of time" do
     before(:all) do
-      @no_rush = Breakfast.new(time: 120, bagels: 13, porridge: 8)
-      @porridge = @no_rush.porridge
+      @no_rush = Breakfast.new(time: 120, food: { bagels: 13, porridge: 8} )
+      @porridge = @no_rush.food[:porridge]
+      @bagels = @no_rush.food[:bagels]
     end
 
     describe "#eat_breakfast" do
@@ -14,7 +15,28 @@ describe Breakfast do
       end
 
       it "should reduce the amount of porridge by one" do
-        expect(@no_rush.porridge).to eq (@porridge-1)
+        expect(@no_rush.food[:porridge]).to eq (@porridge-1)
+      end
+
+      it "should make bagels if there isn't time" do
+        @no_rush.time = 28
+        expect(@no_rush.eat_breakfast).to eq "Delicious bagels consumed!"
+      end
+
+      it "should reduce the amount of porridge by one" do
+        expect(@no_rush.food[:bagels]).to eq (@bagels-1)
+      end
+    end
+  end
+
+  context "when there is no food" do
+    before(:all) do
+      @no_food = Breakfast.new(time: 120, food: { bagels: 0, porridge: 0} )
+    end
+
+    describe "#eat_breakfast" do
+      it 'should state that Tom is starving' do
+        expect(@no_food.eat_breakfast).to eq "Tom is starving :("
       end
     end
   end
